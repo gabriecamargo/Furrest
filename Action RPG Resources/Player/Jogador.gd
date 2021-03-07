5120a114b3115ef6a6368362f22adfc6a81f6807
Gabriel Camargo <gabrielcamargom07@gmail.com>
2021-03-07 02:01:12 -0300

    Movimentação refeita

--- Action RPG Resources/Player/Jogador.gd
+++ Action RPG Resources/Player/Jogador.gd
@@ -1,19 +1,16 @@
 extends KinematicBody2D
 
-var velocidade = Vector2.ZERO
-var spd = 1
+var velocidade = Vector2(0,0)
+var rapidez = 100
+var resultante = Vector2(0,0)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocidade.x = spd
-	elif Input.is_action_pressed("ui_left"):
-		velocidade.x = -spd
-	else:
-		velocidade.x = 0
-	if Input.is_action_pressed("ui_up"):
-		velocidade.y = -spd
-	elif Input.is_action_pressed("ui_down"):
-		velocidade.y = spd
-	else: velocidade.y = 0
+	resultante.x = Input.get_action_strength("direita") - Input.get_action_strength("esquerda") 
+	resultante.y = Input.get_action_strength("baixo") - Input.get_action_strength("cima") 
 	
-	move_and_collide(velocidade)
+	if resultante != Vector2(0, 0):
+		velocidade = resultante * rapidez
+	else:
+		velocidade = Vector2(0,0)
+	
+	move_and_slide(velocidade)
