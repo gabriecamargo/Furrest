bf68211e6c4c2671c0760edc202026aec3c6cbd4
Gabriel Camargo <gabrielcamargom07@gmail.com>
2021-03-10 00:20:15 -0300

    Bug na movimentação corrigido
    Agora o jogador não se mexe mais rápido quando se move na diagonal

--- Action RPG Resources/Player/Jogador.gd
+++ Action RPG Resources/Player/Jogador.gd
@@ -1,16 +1,18 @@
 extends KinematicBody2D
 
-var velocidade = Vector2(0,0)
+var velocidade = Vector2.ZERO
 var rapidez = 100
-var resultante = Vector2(0,0)
 
 func _physics_process(delta):
+	
+	var resultante = Vector2.ZERO #
 	resultante.x = Input.get_action_strength("direita") - Input.get_action_strength("esquerda") 
 	resultante.y = Input.get_action_strength("baixo") - Input.get_action_strength("cima") 
+	resultante = resultante.normalized()
 	
-	if resultante != Vector2(0, 0):
+	if resultante != Vector2.ZERO:
 		velocidade = resultante * rapidez
 	else:
-		velocidade = Vector2(0,0)
+		velocidade = Vector2.ZERO
 	
 	move_and_slide(velocidade)
