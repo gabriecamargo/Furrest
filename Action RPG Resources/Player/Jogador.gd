9d60725047963da877a8d20bcdab9a5f29ccb107
Gabriel Camargo <gabrielcamargom07@gmail.com>
2021-03-14 17:44:38 -0300

    Animações pra todas as direções implementadas
    agora sim

--- Action RPG Resources/Player/Jogador.gd
+++ Action RPG Resources/Player/Jogador.gd
@@ -6,7 +6,9 @@ const ATRITO = ACELERACAO # quanto maior o valor, menor o efeito
 
 var velocidade = Vector2.ZERO
 
-onready var animador = $Animador
+onready var animador = $PlayerAnimacao
+onready var posicao = $AnimationTree
+onready var estado = posicao.get("parameters/playback")
 
 func _physics_process(delta):
 	var resultante = Vector2.ZERO #
@@ -15,12 +17,12 @@ func _physics_process(delta):
 	resultante = resultante.normalized()
 	
 	if resultante != Vector2.ZERO:
-		if resultante.x > 0: $Sprite.set_flip_h(false)
-		else: $Sprite.set_flip_h(true)
-		animador.play("run_lado")
+		posicao.set("parameters/Idle/blend_position", resultante)
+		posicao.set("parameters/Run/blend_position", resultante)
+		estado.travel("Run")
 		velocidade = velocidade.move_toward(resultante * VELOCIDADE_MAX, ACELERACAO * delta) # a velocidade vai 
 	else:
-		animador.play("idle_lado")
+		estado.travel("Idle")
 		velocidade = velocidade.move_toward(Vector2.ZERO, ATRITO * delta)
 	
 	velocidade = move_and_slide(velocidade)
